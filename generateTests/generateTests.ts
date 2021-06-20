f0d48742faa8ff65f683ddd9dcbbb291e070a443
Léo Salé <leo.sale72@gmail.com>
2021-06-20 21:10:32 +0200

    create challenge's dir if not present

--- generateTests/generateTests.ts
+++ generateTests/generateTests.ts
@@ -1,35 +1,24 @@
 import ejs from 'ejs'
 import path from 'path'
-import {readdirSync, writeFileSync} from "fs"
+import {existsSync, mkdirSync, readdirSync, writeFileSync} from "fs"
 
 import {languages} from "../languages"
 
 const languagesTemplate = Object.keys(languages).map(language => ({language, template: require(`./${language}Template`).template}))
 
-const challenges = readdirSync(path.join(__dirname, 'challenges')).map(challenge => path.join(__dirname, 'challenges', challenge))
+const challenges = readdirSync(path.join(__dirname, 'challenges')).map(challenge => ({name: challenge.split('.')[0],spec: path.join(__dirname, 'challenges', challenge)}))
 
-challenges.forEach(challenge => {
-  const testsSpec = require(challenge)
-  console.log(testsSpec)
+challenges.forEach(({name, spec}) => {
+  const dirPath = path.join(__dirname, `../challenges/${name}`)
+  if (!existsSync(dirPath)) {
+    mkdirSync(dirPath)
+  }
+  const testsSpec = require(spec)
   languagesTemplate.forEach(({language, template}) => {
     let data = {...testsSpec, template}
-    console.log("DATA", data)
     ejs.renderFile(path.join(__dirname, "index.ejs"), data, (err, str) => {
-        console.log(str)
         console.log(err)
-        writeFileSync(path.join(__dirname, `../challenges/add/index.${language}`), str)
+        writeFileSync(path.join(__dirname, `../challenges/${name}/index.${language}`), str)
       })
   })
 })
-
-// const data = {tests: [{functionName: "add", params: ["2", "3"], expected: "5"}], template: jsTemplate}
-// let template = ejs.renderFile(path.join(__dirname, "index.ejs"), data, (err, str) => {
-//   console.log(str)
-//   writeFileSync(path.join(__dirname, "../challenges/add/index.js"), str)
-// })
-
-// const data2 = {tests: [{functionName: "add", params: ["2", "3"], expected: "5"}], template: pyTemplate}
-// let template2 = ejs.renderFile(path.join(__dirname, "index.ejs"), data2, (err, str) => {
-//   console.log(str)
-//   fs.writeFileSync(path.join(__dirname, "../challenges/add/index.py"), str)
-// })
