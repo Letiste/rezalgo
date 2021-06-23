cd4da7127a0aa90064fbb66a80d01fbd89797bfc
Léo Salé <leo.sale72@gmail.com>
2021-06-23 19:23:05 +0200

    promisify challenges creation and run it at start

--- src/generateChallenges.ts
+++ src/generateChallenges.ts
@@ -6,43 +6,55 @@ import {languages} from "../schemas"
 
 const languagesMap = Object.keys(languages).map(language => ({language, languageMap: require(`./languagesMap/${language}Template`).languageMap}))
 
-const challenges = readdirSync(path.join(__dirname, 'challenges')).map(challenge => ({name: challenge.split('.')[0],spec: path.join(__dirname, 'challenges', challenge)}))
+async function generateChallenges() {
+  console.log("Creating challenges...")
+  const challenges = readdirSync(path.join(__dirname, 'challenges')).map(challenge => ({name: challenge.split('.')[0],spec: path.join(__dirname, 'challenges', challenge)}))
+  const promises: Promise<void>[] = []
+  challenges.forEach(({name, spec}) => {
+    const challengesSpec = require(spec)
+    
+    promises.push(generateHelpers(challengesSpec, name))
+    promises.concat(generateTests(challengesSpec, name))
+  })
+  
+  await Promise.all(promises)
+  console.log("Challenges created")
+}
 
-challenges.forEach(({name, spec}) => {
-  const challengesSpec = require(spec)
-  
-  generateHelpers(challengesSpec, name)
-  generateTests(challengesSpec, name)
-  
-})
+generateChallenges()
 
 function generateHelpers(challengesSpec: any, name: string) {
   const dirPath = path.join(__dirname, `../dist/helpers/${name}`)
-  console.log(dirPath)
   if (!existsSync(dirPath)) {
-    console.log("create dir")
     mkdirSync(dirPath, {recursive: true})
   }
   let data = {...challengesSpec, helpers: languagesMap}
-  console.log(data)
-  ejs.renderFile(path.join(__dirname, "./templates/helpers.ejs"), data, (err, str) => {
-    console.log(err)
-    writeFileSync(path.join(__dirname, `../dist/helpers/${name}/index.ts`), str)
+  return new Promise<void>((resolve, reject) => {ejs.renderFile(path.join(__dirname, "./templates/helpers.ejs"), data, (err, str) => {
+    if (err) {
+      reject(err)
+    }
+    resolve(writeFileSync(path.join(__dirname, `../dist/helpers/${name}/index.ts`), str))
   })
+})
 }
 
 function generateTests(challengesSpec: any, name: string) {
   const dirPath = path.join(__dirname, `../dist/tests/${name}`)
-  console.log(dirPath)
   if (!existsSync(dirPath)) {
-    console.log("create dir")
     mkdirSync(dirPath, {recursive: true})
   }
+  const promises: Promise<void>[] = []
   languagesMap.forEach(({language, languageMap}) => {
     let data = {...challengesSpec, languageMap}
-    ejs.renderFile(path.join(__dirname, "./templates/test.ejs"), data, (err, str) => {
-        console.log(err)
-        writeFileSync(path.join(__dirname, `../dist/tests/${name}/test.${language}`), str)
+    let promise = new Promise<void>((resolve, reject) => {
+      ejs.renderFile(path.join(__dirname, "./templates/test.ejs"), data, (err, str) => {
+        if (err) {
+          reject(err)
+        }
+        resolve(writeFileSync(path.join(__dirname, `../dist/tests/${name}/test.${language}`), str))
       })
+    })
+    promises.push(promise)
   })
+  return promises
 }
