0d836107a2dd1be2395a06ed90a38f4d0596d58d
Léo Salé <leo.sale72@gmail.com>
2021-07-10 10:46:15 +0200

    validate challengesjson against schema

---
+++ commands/validateChallenges/index.ts
@@ -0,0 +1,17 @@
+import Ajv from "ajv";
+import { readdirSync } from "fs";
+import path from "path";
+const ajv = new Ajv();
+
+import * as schema from '../../challenges.schema.json';
+
+const challenges = readdirSync(path.join(__dirname, '../../challenges')).map((file) => [require(path.join(__dirname, '../../challenges', file)), file]);
+
+const validate = ajv.compile(schema)
+
+for (const [challenge, file] of challenges) {
+  let valid = validate(challenge)
+  if (!valid) {
+    throw new Error(`Challenge ${file} is not valid. Property ${validate.errors![0].instancePath} ${validate.errors![0].message}.`)
+  }
+}
